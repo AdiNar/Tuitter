ccbabd01bc6769f05370ceadaa7133818325513c
Adrian Naruszko <naruszko.adrian@gmail.com>
2017-06-15 16:37:04 +0200

    Statistics added.

--- twitter/twits/models.py
+++ twitter/twits/models.py
@@ -12,10 +12,15 @@ class Twit(models.Model):
     text = models.CharField(max_length=100)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_on = models.DateTimeField(editable=False, auto_now_add=True)
+    text_length = models.IntegerField(default=0)
     
     def __str__(self):
         return 'Twit: "{0}" created by {1} on {2}'.format(
             self.text, self.created_by, self.get_date())
+
+    def save(self, *args, **kwargs):
+        self.text_length = len(self.text)
+        super(Twit, self).save(*args, **kwargs)
 
     # Returns nice looking date of creation. For shorter
     # periods returns hour, day of week,
@@ -26,8 +31,6 @@ class Twit(models.Model):
         time_passed = now_timezone_aware - self.created_on
         
         if time_passed > timedelta(weeks=1):
-            if time_passed > timedelta(years=1):
-                result_format += 'o, '
             result_format += 'j b'
             
         else:
